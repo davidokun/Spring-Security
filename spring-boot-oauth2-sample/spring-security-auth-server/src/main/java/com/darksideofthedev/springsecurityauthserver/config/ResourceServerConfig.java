1707a1e423a1492b87521559f392278d69054416
davidokun <davidokun99@gmail.com>
2017-11-21 23:10:19 -0500

    Changing role from ADMIN to USER

--- spring-boot-oauth2-sample/spring-security-auth-server/src/main/java/com/darksideofthedev/springsecurityauthserver/config/ResourceServerConfig.java
+++ spring-boot-oauth2-sample/spring-security-auth-server/src/main/java/com/darksideofthedev/springsecurityauthserver/config/ResourceServerConfig.java
@@ -36,6 +36,6 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                 .inMemoryAuthentication()
                 .withUser("dark")
                 .password("side")
-                .roles("ADMIN");
+                .roles("USER");
     }
 }

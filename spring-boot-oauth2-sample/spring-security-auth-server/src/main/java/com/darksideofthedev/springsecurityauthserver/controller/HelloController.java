17239a690248c7ea71368a7de0cd1c6966199d5a
davidokun <davidokun99@gmail.com>
2017-11-21 23:09:25 -0500

    Adding new endpoint for principal and updating client config Uri

--- spring-boot-oauth2-sample/spring-security-auth-server/src/main/java/com/darksideofthedev/springsecurityauthserver/controller/HelloController.java
+++ spring-boot-oauth2-sample/spring-security-auth-server/src/main/java/com/darksideofthedev/springsecurityauthserver/controller/HelloController.java
@@ -4,9 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 @RequestMapping("/v1/hello")
 public class HelloController {
+
+    @GetMapping("/principal")
+    public Principal user(Principal principal) {
+
+        return principal;
+    }
 
     @GetMapping
     public String sayHello() {

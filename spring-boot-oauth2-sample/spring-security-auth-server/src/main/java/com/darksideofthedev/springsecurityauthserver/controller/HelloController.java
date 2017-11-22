4db2c93235dfe33a1fd27e071d39101cd8a05e4a
davidokun <davidokun99@gmail.com>
2017-11-21 20:54:55 -0500

    Adding Resource endpoint

---
+++ spring-boot-oauth2-sample/spring-security-auth-server/src/main/java/com/darksideofthedev/springsecurityauthserver/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.darksideofthedev.springsecurityauthserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/hello")
+public class HelloController {
+
+    @GetMapping
+    public String sayHello() {
+
+        return "Hello World";
+    }
+}

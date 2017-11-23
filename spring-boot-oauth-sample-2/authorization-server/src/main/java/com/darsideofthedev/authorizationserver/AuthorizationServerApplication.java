5b33df9c863d5532c03d0d0e601aca6eae6ab73f
davidokun <davidokun99@gmail.com>
2017-11-22 21:19:36 -0500

    Adding Authorization Server config

--- spring-boot-oauth-sample-2/authorization-server/src/main/java/com/darsideofthedev/authorizationserver/AuthorizationServerApplication.java
+++ spring-boot-oauth-sample-2/authorization-server/src/main/java/com/darsideofthedev/authorizationserver/AuthorizationServerApplication.java
@@ -1,12 +1,25 @@
 package com.darsideofthedev.authorizationserver;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.security.Principal;
 
 @SpringBootApplication
 public class AuthorizationServerApplication {
 
+	private static final Log logger = LogFactory.getLog(AuthorizationServerApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(AuthorizationServerApplication.class, args);
+	}
+
+	public Principal user(Principal user) {
+
+		logger.info("AS /user has been called");
+		logger.debug("user info: " + user.toString());
+		return user;
 	}
 }

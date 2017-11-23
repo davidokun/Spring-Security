55915ed16238ef1a7a81d9d0c9188252e35e547b
davidokun <davidokun99@gmail.com>
2017-11-22 22:21:07 -0500

    Fixing configuration

--- spring-boot-oauth-sample-2/authorization-server/src/main/java/com/darsideofthedev/authorizationserver/AuthorizationServerApplication.java
+++ spring-boot-oauth-sample-2/authorization-server/src/main/java/com/darsideofthedev/authorizationserver/AuthorizationServerApplication.java
@@ -4,10 +4,15 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
 @SpringBootApplication
+@EnableResourceServer
+@RestController
 public class AuthorizationServerApplication {
 
 	private static final Log logger = LogFactory.getLog(AuthorizationServerApplication.class);
@@ -16,6 +21,7 @@ public class AuthorizationServerApplication {
 		SpringApplication.run(AuthorizationServerApplication.class, args);
 	}
 
+	@GetMapping("/user")
 	public Principal user(Principal user) {
 
 		logger.info("AS /user has been called");

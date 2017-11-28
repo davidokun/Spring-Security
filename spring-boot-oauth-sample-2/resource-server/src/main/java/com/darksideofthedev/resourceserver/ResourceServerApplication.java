4c7c557a921fb16ea1526fca26e427f62360e01c
davidokun <davidokun99@gmail.com>
2017-11-27 19:24:18 -0500

    Adding authorization check to each endpoint

--- spring-boot-oauth-sample-2/resource-server/src/main/java/com/darksideofthedev/resourceserver/ResourceServerApplication.java
+++ spring-boot-oauth-sample-2/resource-server/src/main/java/com/darksideofthedev/resourceserver/ResourceServerApplication.java
@@ -2,6 +2,8 @@ package com.darksideofthedev.resourceserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,7 @@ import java.util.Map;
 @SpringBootApplication
 @RestController
 @EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServerApplication {
 
 	public static void main(String[] args) {
@@ -24,19 +27,23 @@ public class ResourceServerApplication {
 
 	private String message = "Hello World!";
 
+
 	@GetMapping("/")
+	@PreAuthorize("hasRole('ROLE_RS_READ')")
 	public Map<String, String> home() {
 
 		return Collections.singletonMap("message", message);
 	}
 
 	@PostMapping("/")
+	@PreAuthorize("hasRole('ROLE_RS_WRITE')")
 	public void updateMessage(@RequestBody String message) {
 
 		this.message = message;
 	}
 
 	@GetMapping("/user")
+	@PreAuthorize("#oauth2.hasScope('resource-server-read')")
 	public Map<String, String> user(Principal user) {
 
 		return Collections.singletonMap("message", "user is: " + user.toString());
